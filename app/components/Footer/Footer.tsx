6fa044b6319cd8b489559f61288e662c9c2810dc
Greg Lorenzen <gregorylorenzen@outlook.com>
2024-04-17 22:25:11 -0700

    Add Footer component and styles

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,7 +1,30 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faCopyright } from "@fortawesome/free-solid-svg-icons";
+import styles from "./Footer.module.css";
+import Container from "../Container/Container";
+
 export default function Footer() {
+    const year = new Date().getFullYear();
+
     return (
-      <footer>
-        <h1>Website Footer</h1>
-      </footer>
+        <footer className={styles.footer}>
+            <Container>
+                <div className={styles.footerContent}>
+                    <p>
+                        <FontAwesomeIcon icon={faCopyright} /> {year} Greg
+                        Lorenzen
+                    </p>
+                    <a
+                        href="https://github.com/glorenzen"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className={styles.github}
+                    >
+                        <FontAwesomeIcon icon={faGithub} size="xl" />
+                    </a>
+                </div>
+            </Container>
+        </footer>
     );
-  }
+}
