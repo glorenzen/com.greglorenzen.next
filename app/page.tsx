cfa2d05b5bde740b9053405755474c9c5d0cf5fa
Greg Lorenzen <gregorylorenzen@outlook.com>
2024-04-17 22:25:08 -0700

    Update home section styles in page.module.css and page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
     return (
         <div>
             <Header />
-            <main>
+            <main className={styles.home}>
                 <Container>
                     <section
                         className={styles.hero}
