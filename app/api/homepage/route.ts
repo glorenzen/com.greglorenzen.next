90180087a3c930c7d9d3e90105dc8262e0b18b0d
Greg Lorenzen <gregorylorenzen@outlook.com>
2024-04-19 17:51:03 -0700

    Add revalidatePath function to homepage API route

--- app/api/homepage/route.ts
+++ app/api/homepage/route.ts
@@ -1,6 +1,9 @@
+import { revalidatePath } from "next/cache";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
+    revalidatePath(request.url);
+
     const response = await fetch(
         `${process.env.NEXT_PUBLIC_SERVER_URL}/api/home-page?populate=heroBackgroundImage`,
         {
