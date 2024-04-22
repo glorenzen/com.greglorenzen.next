ff13f691c7e1b88f0cda7a8c0d979b934642fc86
Greg Lorenzen <gregorylorenzen@outlook.com>
2024-04-22 16:04:01 -0700

    Add wide prop in Container component

--- app/components/Container/Container.tsx
+++ app/components/Container/Container.tsx
@@ -1,11 +1,17 @@
-import styles from "./Container.module.css";
+import React from 'react';
+import styles from './Container.module.css';
 
 interface ContainerProps {
     children: React.ReactNode;
+    wide?: boolean;
 }
 
-const Container: React.FC<ContainerProps> = ({ children }) => {
-    return <div className={styles.container}>{children}</div>;
-};
+const Container: React.FC<ContainerProps> = ({ children, wide = false }) => {
+    return (
+        <div className={`${styles.container} ${wide ? styles.wide : ''}`}>
+            {children}
+        </div>
+    );
+}
 
 export default Container;
