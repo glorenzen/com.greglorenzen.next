e1cc7b8b8bfd07c8f2e08177e9dfa1033b4bc110
Greg Lorenzen <gregorylorenzen@outlook.com>
2024-04-22 15:11:30 -0700

    Update resume page layout and add css module file

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,11 +1,17 @@
+import Container from "../components/Container/Container";
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
+import styles from "./resume.module.css";
 
 export default function Resume() {
     return (
         <div>
             <Header />
-            <h1>Resume Page</h1>
+            <main className={styles.resume}>
+                <Container>
+                    <h1>Resume</h1>
+                </Container>
+            </main>
             <Footer />
         </div>
     );
