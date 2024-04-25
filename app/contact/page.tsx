b56a02673fdc96b8bc921ca15112a818993542be
Greg Lorenzen <gregorylorenzen@outlook.com>
2024-04-25 16:17:26 -0700

    Remove Header and Footer components from Contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,12 +1,7 @@
-import Footer from "../components/Footer/Footer";
-import Header from "../components/Header/Header";
-
 export default function Contact() {
     return (
         <div>
-            <Header />
             <h1>Contact Page</h1>
-            <Footer />
         </div>
     );
 }
