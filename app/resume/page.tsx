321fda68219c466d10f5405fb93bc3d960996e3c
Greg Lorenzen <gregorylorenzen@outlook.com>
2024-04-26 11:22:24 -0700

    Refactor resume page to use async/await for data fetching and update UI components

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,51 +1,55 @@
-"use client";
-
 // Import necessary libraries and components
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Container from "../components/Container/Container";
-import Footer from "../components/Footer/Footer";
-import Header from "../components/Header/Header";
-import Timeline, { TimelineItem } from "../components/Timeline/Timeline";
 import styles from "./resume.module.css";
+import Markdown from "react-markdown";
+import TitleHeading from "../components/TitleHeading/TitleHeading";
+import { revalidatePath } from "next/cache";
 
-export default function Resume() {
-    // Create state variables with placeholder data
-    const [title, setTitle] = useState("Resume");
-    const [timelineItems, setTimelineItems] = useState<TimelineItem[]>();
-    const [skills, setSkills] = useState<string[]>([
-        "JavaScript",
-        "React",
-        "Node.js",
-    ]);
+interface ExperienceItem {
+    dates: string;
+    companyName: string;
+    jobTitle: string;
+    jobDescription: string;
+}
 
-    // Fetch data from Strapi
-    useEffect(() => {
-        fetch("/api/jobs")
-            .then((response) => response.json())
-            .then((data) =>
-                setTimelineItems(
-                    data.map((item: any) => ({
-                        title: item.attributes.dates,
-                        cardTitle: item.attributes.companyName,
-                        cardSubtitle: item.attributes.jobTitle,
-                        timelineContent: item.attributes.jobDescription,
-                    }))
-                )
-            );
-    }, []);
+interface Data {
+    experienceItems: ExperienceItem[];
+    skills: string[];
+    globalData: any;
+}
+
+export default async function Resume() {
+    const data = await getData();
+
+    const { experienceItems, skills, globalData: global } = data;
 
     return (
         <div>
-            <Header />
-            <div className={styles.hero}>
-                <h1>{title}</h1>
-            </div>
+            <TitleHeading
+                title="Resume"
+                backgroundImage={`${process.env.NEXT_PUBLIC_SERVER_URL}${global.attributes.titleHeadingBackground.data.attributes.url}`}
+                
+            />
             <main className={styles.resume}>
-                <Container wide>
+                <Container>
                     <div className={styles.grid}>
-                        <div className={styles.timeline}>
-                            <h2>Resume</h2>
-                            {timelineItems ? <Timeline items={timelineItems} /> : null}
+                        <div className={styles.experience}>
+                            <h2>Experience</h2>
+                            {experienceItems
+                                ? experienceItems.map(
+                                      (item: ExperienceItem, index: number) => (
+                                          <div key={index}>
+                                              <h3>{item.companyName}</h3>
+                                              <h4>{item.jobTitle}</h4>
+                                              <p>{item.dates}</p>
+                                              <Markdown>
+                                                  {item.jobDescription}
+                                              </Markdown>
+                                          </div>
+                                      )
+                                  )
+                                : null}
                         </div>
                         <div className={styles.skills}>
                             <h2>Skills</h2>
@@ -58,7 +62,36 @@ export default function Resume() {
                     </div>
                 </Container>
             </main>
-            <Footer />
         </div>
     );
+}
+
+async function getData(): Promise<Data> {
+    const jobUrl = `${process.env.NEXT_PUBLIC_SERVER_URL}/api/jobs?populate=jobDescription`;
+    const globalUrl = `${process.env.NEXT_PUBLIC_SERVER_URL}/api/global?populate=*`;
+
+    revalidatePath(jobUrl);
+    revalidatePath(globalUrl);
+
+    const jobsRes = await fetch(jobUrl, {
+        headers: {
+            Authorization: `Bearer ${process.env.STRAPI_API_TOKEN}`,
+        },
+    });
+
+    const globalRes = await fetch(globalUrl, {
+        headers: {
+            Authorization: `Bearer ${process.env.STRAPI_API_TOKEN}`,
+        },
+    });
+
+    const { data: globalData } = await globalRes.json();
+
+    const { data: jobData } = await jobsRes.json();
+
+    return {
+        experienceItems: jobData.map((job: any) => ({ ...job.attributes })),
+        skills: ["Javascript", "NodeJS", "React"],
+        globalData,
+    };
 }
