747cacc77670372478c42934ff41a04f9bd5d99c
Greg Lorenzen <gregorylorenzen@outlook.com>
2024-04-29 15:46:51 -0700

    Update container styles and props in Container module and component

--- app/components/Container/Container.tsx
+++ app/components/Container/Container.tsx
@@ -3,12 +3,12 @@ import styles from './Container.module.css';
 
 interface ContainerProps {
     children: React.ReactNode;
-    wide?: boolean;
+    thin?: boolean;
 }
 
-const Container: React.FC<ContainerProps> = ({ children, wide = false }) => {
+const Container: React.FC<ContainerProps> = ({ children, thin = false }) => {
     return (
-        <div className={`${styles.container} ${wide ? styles.wide : ''}`}>
+        <div className={`${styles.container} ${thin ? styles.thin : ''}`}>
             {children}
         </div>
     );
