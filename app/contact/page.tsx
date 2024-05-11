4469486cd00e041902ef5c78625a4d6f1e892ca2
Greg Lorenzen <gregorylorenzen@outlook.com>
2024-05-10 22:12:09 -0700

    refactor: Update contact page layout

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,25 +1,43 @@
 import TitleHeading from "../components/TitleHeading/TitleHeading";
+import ContactForm from "../components/ContactForm/ContactForm";
+import styles from "./contact.module.css";
 import { getGlobal } from "../utils/getGlobal";
+import Container from "../components/Container/Container";
+import { getContactPage } from "../utils/getContactPage";
 
 export default async function Contact() {
     const {
         globalData: { titleHeadingBackground },
+        contactPageData: { heading, ctaText },
     } = await getData();
 
     return (
-        <>
+        <div className={styles.contact}>
             <TitleHeading
                 backgroundImage={`${process.env.NEXT_PUBLIC_SERVER_URL}${titleHeadingBackground}`}
                 title="Contact"
             />
-        </>
+            <Container>
+                <div className={styles.twoColumn}>
+                    <div className={styles.leftColumn}>
+                        <h1>{heading}</h1> {/* Set the heading value */}
+                        <p>{ctaText}</p>
+                    </div>
+                    <div className={styles.rightColumn}>
+                        <ContactForm />
+                    </div>
+                </div>
+            </Container>
+        </div>
     );
 }
 
 async function getData() {
     const globalData = await getGlobal();
+    const contactPageData = await getContactPage();
 
     return {
         globalData,
+        contactPageData,
     };
 }
