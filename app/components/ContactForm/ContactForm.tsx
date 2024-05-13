36f5fba51916e4e082236b603ebd2e06476648e8
Greg Lorenzen <gregorylorenzen@outlook.com>
2024-05-13 14:17:06 -0700

    refactor: Update ContactForm layout and styles
    feat: Add phone number field and form validation
    
    This commit updates the layout and styles of the ContactForm component to improve the user experience. It introduces a new CSS class called "row" to create a responsive two-column layout for the form fields. It also adds styling to the form fields, including padding, border radius, and border color. Additionally, it adds a "required" class to indicate required fields with a red color. These changes enhance the visual appeal and usability of the contact form.

--- app/components/ContactForm/ContactForm.tsx
+++ app/components/ContactForm/ContactForm.tsx
@@ -17,9 +17,10 @@ const ContactForm: React.FC = () => {
                 New contact form submission from greglorenzen.com:
                 Name: ${rawFormData.firstName} ${rawFormData.lastName}
                 Email: ${rawFormData.email}
+                Phone: ${rawFormData.phone}
                 Message: ${rawFormData.message}
             `,
-            'h:Reply-To': rawFormData.email as string
+            "h:Reply-To": rawFormData.email as string,
         });
 
         // Send email to the user
@@ -28,23 +29,55 @@ const ContactForm: React.FC = () => {
             from: `Greg Lorenzen <postmaster@${process.env.MAILGUN_DOMAIN}>`,
             subject: "Thanks for reaching out!",
             text: "I've received your email and I will get back to you as soon as possible!",
-            'h:Reply-To': 'gregorylorenzen@gmail.com'
+            "h:Reply-To": "gregorylorenzen@gmail.com",
         });
     };
 
     return (
         <form className={styles.contactForm} action={handleSubmit}>
-            <label htmlFor="firstName">First Name</label>
-            <input id="firstName" name="firstName" type="text" />
+            <div className={styles.row}>
+                <div className={styles.formField}>
+                    <label htmlFor="firstName">
+                        First Name <span className={styles.required}>*</span>
+                    </label>
+                    <input
+                        id="firstName"
+                        name="firstName"
+                        type="text"
+                        required
+                    />
+                </div>
 
-            <label htmlFor="lastName">Last Name</label>
-            <input id="lastName" name="lastName" type="text" />
+                <div className={styles.formField}>
+                    <label htmlFor="lastName">
+                        Last Name <span className={styles.required}>*</span>
+                    </label>
+                    <input id="lastName" name="lastName" type="text" required />
+                </div>
+            </div>
 
-            <label htmlFor="email">Email</label>
-            <input id="email" name="email" type="email" />
+            <div className={styles.row}>
+                <div className={styles.formField}>
+                    <label htmlFor="email">
+                        Email <span className={styles.required}>*</span>
+                    </label>
+                    <input id="email" name="email" type="email" required />
+                </div>
 
-            <label htmlFor="message">Message</label>
-            <textarea id="message" name="message" />
+                <div className={styles.formField}>
+                    <label htmlFor="phone">
+                        Phone <span className={styles.required}>*</span>
+                    </label>
+                    <input id="phone" name="phone" type="tel" required />
+                </div>
+            </div>
+
+            <div className={styles.formField}>
+                <label htmlFor="message">
+                    Message <span className={styles.required}>*</span>
+                </label>
+                <textarea id="message" name="message" required />
+            </div>
 
             <button type="submit">Submit</button>
         </form>
