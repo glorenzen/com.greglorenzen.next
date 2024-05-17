7716e33c96b8eaa410653fa2ebbd36eea66a9cb5
Greg Lorenzen <gregorylorenzen@outlook.com>
2024-05-17 14:25:05 -0700

    refactor: Update ContactForm to include reCAPTCHA for form submission

--- app/components/ContactForm/ContactForm.tsx
+++ app/components/ContactForm/ContactForm.tsx
@@ -1,43 +1,12 @@
+"use client";
+
 import React from "react";
-import styles from "./ContactForm.module.css";
-import { sendEmail } from "../../utils/mailgun";
-import { redirect } from "next/navigation";
+import ReCAPTCHA from "react-google-recaptcha";
 import Button from "../Button/Button";
+import styles from "./ContactForm.module.css";
+import handleSubmit from "./handleSubmit";
 
 const ContactForm: React.FC = () => {
-    const handleSubmit = async (formData: FormData) => {
-        "use server";
-
-        const rawFormData = Object.fromEntries(formData);
-
-        // Send notification email to admin
-        await sendEmail({
-            to: process.env.ADMIN_EMAIL as string,
-            from: `Greg Lorenzen Website <postmaster@${process.env.MAILGUN_DOMAIN}>`,
-            subject: "New contact form submission (greglorenzen.com)",
-            text: `
-                New contact form submission from greglorenzen.com:
-                Name: ${rawFormData.firstName} ${rawFormData.lastName}
-                Email: ${rawFormData.email}
-                Phone: ${rawFormData.phone}
-                Message: ${rawFormData.message}
-            `,
-            "h:Reply-To": rawFormData.email as string,
-        });
-
-        // Send email to the user
-        await sendEmail({
-            to: rawFormData.email as string,
-            from: `Greg Lorenzen <postmaster@${process.env.MAILGUN_DOMAIN}>`,
-            subject: "Thanks for reaching out!",
-            text: "I've received your email and I will get back to you as soon as possible!",
-            "h:Reply-To": "gregorylorenzen@gmail.com",
-        });
-
-        // Redirect to the thank you page
-        redirect("/thank-you");
-    };
-
     return (
         <form className={styles.contactForm} action={handleSubmit}>
             <div className={styles.row}>
@@ -83,6 +52,25 @@ const ContactForm: React.FC = () => {
                 </label>
                 <textarea id="message" name="message" required />
             </div>
+
+            <ReCAPTCHA
+                sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY as string}
+                onChange={(value) => {
+                    const element = document.getElementById(
+                        "recaptchaResponse"
+                    ) as HTMLInputElement;
+                    if (element) {
+                        element.value = value ?? "";
+                    }
+                }}
+                style={{ marginBottom: "30px" }}
+            />
+
+            <input
+                type="hidden"
+                id="recaptchaResponse"
+                name="recaptchaResponse"
+            />
 
             <Button type="submit" variant="primary">
                 Submit
---
+++ app/components/ContactForm/handleSubmit.ts
@@ -0,0 +1,39 @@
+"use server";
+
+import { sendEmail } from "@/app/utils/mailgun";
+import { redirect } from "next/navigation";
+
+const handleSubmit = async (formData: FormData) => {
+    const rawFormData = Object.fromEntries(formData);
+
+    console.log(rawFormData);
+
+    // Send notification email to admin
+    await sendEmail({
+        to: process.env.ADMIN_EMAIL as string,
+        from: `Greg Lorenzen Website <postmaster@${process.env.MAILGUN_DOMAIN}>`,
+        subject: "New contact form submission (greglorenzen.com)",
+        text: `
+            New contact form submission from greglorenzen.com:
+            Name: ${rawFormData.firstName} ${rawFormData.lastName}
+            Email: ${rawFormData.email}
+            Phone: ${rawFormData.phone}
+            Message: ${rawFormData.message}
+        `,
+        "h:Reply-To": rawFormData.email as string,
+    });
+
+    // Send email to the user
+    await sendEmail({
+        to: rawFormData.email as string,
+        from: `Greg Lorenzen <postmaster@${process.env.MAILGUN_DOMAIN}>`,
+        subject: "Thanks for reaching out!",
+        text: "I've received your email and I will get back to you as soon as possible!",
+        "h:Reply-To": "gregorylorenzen@gmail.com",
+    });
+
+    // Redirect to the thank you page
+    redirect("/thank-you");
+};
+
+export default handleSubmit;
