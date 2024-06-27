245d8105d418b9e1c3ff13a421385ade909a130c
Greg Lorenzen <gregorylorenzen@outlook.com>
2024-06-26 18:25:31 -0700

    Add basic lightbox functionality

--- app/components/MasonryGallery/MasonryGallery.tsx
+++ app/components/MasonryGallery/MasonryGallery.tsx
@@ -1,27 +1,57 @@
-import React from "react";
+"use client";
+
 import Image from "next/image";
+import React, { useState } from "react";
 import styles from "./MasonryGallery.module.css";
+import { faSearchPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 interface MasonryGalleryProps {
     images: { url: string; alt: string }[];
 }
 
 const MasonryGallery: React.FC<MasonryGalleryProps> = ({ images }) => {
+    const [lightbox, setLightbox] = useState<{
+        url: string;
+        alt: string;
+    } | null>(null);
+
+    const openLightbox = (image: { url: string; alt: string }) => {
+        setLightbox(image);
+    };
+
+    const closeLightbox = () => {
+        setLightbox(null);
+    };
+
     return (
-        <div className={styles.masonry}>
-            {images.map((image, index) => (
-                <div className={styles.masonryItem} key={index}>
-                    <Image
+        <>
+            <div className={styles.masonry}>
+                {images.map((image, index) => (
+                    <div
+                        className={styles.masonryItem}
                         key={index}
-                        src={image.url}
-                        width={0}
-                        height={0}
-                        alt={image.alt}
-                        sizes="100vw"
-                    />
+                        onClick={() => openLightbox(image)}
+                    >
+                        <Image
+                            src={image.url}
+                            width={0}
+                            height={0}
+                            sizes="100vw"
+                            alt={image.alt}
+                        />
+                        <div className={styles.iconOverlay}>
+                            <FontAwesomeIcon icon={faSearchPlus} />
+                        </div>
+                    </div>
+                ))}
+            </div>
+            {lightbox && (
+                <div className={styles.lightboxOverlay} onClick={closeLightbox}>
+                    <img src={lightbox.url} alt={lightbox.alt} />
                 </div>
-            ))}
-        </div>
+            )}
+        </>
     );
 };
 
